a3b13d64d49b36f9dca65fd26fabb564b690d292
thrasherdustin9904 <thrasher.dustin9904@gmail.com>
2025-11-29 13:31:50 -0500

    Delete products.js

--- products.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { addProduct, getProducts } from '../controllers/products.js';
-
-const router = Router();
-
-router.get('/', getProducts);
-router.post('/add', addProduct);
-
-export default router;
